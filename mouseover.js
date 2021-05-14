7822353080b8e64d81059b30ea6cfa99d24ab2db
t00ns <67071434+t00ns@users.noreply.github.com>
2021-05-14 13:52:18 -0400

    Create mouseover.js

---
+++ mouseover.js
@@ -0,0 +1,15 @@
+// basic code for mouseover background event. DO NOT RUN
+
+
+// var container = document.querySelector('#container');
+
+// container.addEventListener('mouseenter', function(){
+//    this.classList.remove('first');
+//    this.classList.add('second');
+//})
+
+
+//container.addEventListener('mouseleave', function(){
+//    this.classList.add('first');
+//    this.classList.remove('second');
+//})
